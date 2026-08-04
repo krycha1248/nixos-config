8402ceb480736e129494710ba6f422f9def7f9b9
Krystian Włodek <krycha1248@gmail.com>
2026-08-04 22:10:32 +0200

    theming

--- home/krystian.nix
+++ home/krystian.nix
@@ -30,6 +30,8 @@
     playerctl
     mako
     libnotify
+    gparted
+    thunderbird
   ];
 
   xdg.configFile."hypr/hyprland.conf".source =
@@ -64,6 +66,68 @@
       ${pkgs.hyprland}/bin/hyprctl reload || true
     fi
   '';
+
+
+  xdg.userDirs = {
+    enable = true;
+
+    createDirectories = true;
+
+    desktop = "${config.home.homeDirectory}/Desktop";
+    documents = "${config.home.homeDirectory}/Documents";
+    download = "${config.home.homeDirectory}/Downloads";
+    music = "${config.home.homeDirectory}/Music";
+    pictures = "${config.home.homeDirectory}/Pictures";
+    publicShare = "${config.home.homeDirectory}/Public";
+    templates = "${config.home.homeDirectory}/Templates";
+    videos = "${config.home.homeDirectory}/Videos";
+  };
+
+  services.hyprpolkitagent = {
+    enable = true;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.catppuccin-gtk;
+      name = "Catppuccin-Mocha-Standard-Blue-Dark";
+    };
+
+    iconTheme = {
+      package = pkgs.catppuccin-papirus-folders;
+      name = "Papirus-Dark";
+    };
+
+    cursorTheme = {
+      package = pkgs.catppuccin-cursors.mochaDark;
+      name = "catppuccin-mocha-dark-cursors";
+      size = 24;
+    };
+
+    gtk3.extraConfig = {
+      "gtk-application-prefer-dark-theme" = true;
+    };
+
+    gtk4.extraConfig = {
+      "gtk-application-prefer-dark-theme" = true;
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.catppuccin-cursors.mochaDark;
+    name = "catppuccin-mocha-dark-cursors";
+    size = 24;
+  };
+
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
 
 
   # ------------------------------------------------------------
