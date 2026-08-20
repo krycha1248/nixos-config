24a8c9f05c350f45b754446b41a4baf77b4d9d99
Krystian Włodek <krycha1248@gmail.com>
2026-08-20 22:57:13 +0200

    added config for power management

--- modules/desktop/power.nix
+++ modules/desktop/power.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 
 {
+  # ------------------------------------------------------------
+  # Power management
+  # ------------------------------------------------------------
+
+  powerManagement.enable = true;
+  services.thermald.enable = true;
+
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
 
